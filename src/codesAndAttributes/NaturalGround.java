63eefd5ee7f7e139aaacdc459588f8a0b5272be0
J2112O <parabollus@gmail.com>
2016-05-09 20:12:58 -0500

    Added the NaturalGround Class. Since it is just a single int, did not
    use extends from the BasicAttributes Class for it. Just made its own
    
    getter and setter.

---
+++ src/codesAndAttributes/NaturalGround.java
@@ -0,0 +1,25 @@
+/*
+ * Captures the Natural Ground Shot 
+ */
+package codesAndAttributes;
+import java.util.Scanner;
+
+public class NaturalGround {
+
+	Scanner input = new Scanner(System.in);
+	private int naturalGround;
+	
+	public int getNaturalGround() {
+		return naturalGround;
+	}
+	public void setNaturalGround(int naturalGround) {
+		this.naturalGround = naturalGround;
+	}
+	
+	public void naturalGroundDataCollect() {
+		System.out.println("Enter the GPS Point for the Shot: ");
+		this.naturalGround = input.nextInt();
+		input.nextLine();
+		input.close();
+	}
+}
