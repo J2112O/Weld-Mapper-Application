04cccbaacc96328960e0a27da85c7d2074871768
Joshua Otwell <parabollus@gmail.combigdaddy@LE2>
2017-03-08 19:45:27 -0600

    Working on BendCollection.java
    No real changes. Just adding class tags

--- src/collectionActivites/BendCollection.java
+++ src/collectionActivites/BendCollection.java
@@ -4,6 +4,7 @@ package collectionActivites;
  * This class takes care of the client side collection and insertion for a Bend. (Sag, Overbend, PI (left or right))
  */
 import database.DbHelper;
+import sun.rmi.runtime.Log;
 import surveyCodes.Bend;
 
 import java.sql.*;
@@ -11,8 +12,13 @@ import java.util.Scanner;
 
 public class BendCollection {
 
+    private static final String TAG = "DBHelper.class";
+
+    public void bendCollectionClassLogMethod() {
+    }
+
     public static void collectBend(){
-        String dummyStationNumber; // Using this to collect the string Station number prior to converting
+        String dummyStationNumber; // Using this to collect the string Station number prior to converting and inserting
         int mWholeStationNum;
         double mOffsetStationNum;
         int mGpsShot;
